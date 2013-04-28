b143d34586cf61434410642216ac26da5a7bbf67
Rick <rkuhlman@gmail.com>
2013-04-28 14:27:34 -0500

    Adding BusinessCard Quiz Screen

---
+++ QuizIn/QuizIn/Source/Classes/UI/Screens/Quizzes/BusinessCard/QIBusinessCardQuizView.h
@@ -0,0 +1,13 @@
+//
+//  QIBusinessCardQuizView.h
+//  QuizIn
+//
+//  Created by Rick Kuhlman on 4/28/13.
+//  Copyright (c) 2013 Kuhlmanation LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QIBusinessCardQuizView : UIView
+
+@end
---
+++ QuizIn/QuizIn/Source/Classes/UI/Screens/Quizzes/BusinessCard/QIBusinessCardQuizView.m
@@ -0,0 +1,31 @@
+//
+//  QIBusinessCardQuizView.m
+//  QuizIn
+//
+//  Created by Rick Kuhlman on 4/28/13.
+//  Copyright (c) 2013 Kuhlmanation LLC. All rights reserved.
+//
+
+#import "QIBusinessCardQuizView.h"
+
+@implementation QIBusinessCardQuizView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
