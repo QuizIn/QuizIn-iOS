a971c731c54b92fbc56dfbd98863d5dbce7e86b3
Rick <rkuhlman@gmail.com>
2013-05-05 11:57:55 -0500

    More autolayout work on homeView

--- QuizIn/QuizIn/Source/Classes/UI/Screens/Home/QIHomeView.m
+++ QuizIn/QuizIn/Source/Classes/UI/Screens/Home/QIHomeView.m
@@ -12,7 +12,10 @@
 @property(nonatomic, strong) UIView *connectionsQuizImagePreviewCollection;
 @property(nonatomic, strong, readwrite) UIButton *connectionsQuizButton;
 
-@property(nonatomic, strong) NSMutableArray *constraints;
+//constraints
+@property(nonatomic, strong) NSMutableArray *constraintsForTopLevelViews;
+@property(nonatomic, strong) NSMutableArray *constraintsForConnectionsQuizStartView;
+
 
 @end
 
@@ -58,32 +61,48 @@
 - (void)updateConstraints {
   [super updateConstraints];
   if (!self.constraints) {
-    NSDictionary *views = NSDictionaryOfVariableBindings(_connectionsQuizPaperImage,_connectionsQuizBinderImage,_connectionsQuizTitle,_connectionsQuizNumberOfConnectionsLabel,_connectionsQuizImagePreviewCollection,_connectionsQuizButton,_connectionsQuizStartView);
-    NSString *vis = @"V:|-[_connectionsQuizStartView]-|";
-
+    //SuperView Constraints
+    NSDictionary *topLevelViews = NSDictionaryOfVariableBindings(_connectionsQuizStartView);
     
-    self.constraints = [NSMutableArray array];
-    //[self.constraints addObjectsFromArray:hConstraints];
+    NSArray *hConstraintsTopLevelViews =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_connectionsQuizStartView]-|"
+                                            options:NSLayoutFormatAlignAllBaseline
+                                            metrics:nil
+                                              views:topLevelViews];
+    NSArray *vConstraintsTopLevelViews =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-30-[_connectionsQuizStartView]"
+                                            options:NSLayoutFormatAlignAllBaseline
+                                            metrics:nil
+                                              views:topLevelViews];
+    
+    [self.constraintsForTopLevelViews  addObjectsFromArray:hConstraintsTopLevelViews];
+    [self.constraintsForTopLevelViews  addObjectsFromArray:vConstraintsTopLevelViews];
+    [self addConstraints:self.constraintsForTopLevelViews];
+
+    //ConnectionsQuizStartView Constraints
+    NSDictionary *views = NSDictionaryOfVariableBindings(_connectionsQuizPaperImage,_connectionsQuizBinderImage,_connectionsQuizTitle,_connectionsQuizNumberOfConnectionsLabel,_connectionsQuizImagePreviewCollection,_connectionsQuizButton);
+
+    self.constraintsForTopLevelViews = [NSMutableArray array];
     //[self.constraints addObjectsFromArray:@[cn,cn2]];
-    [self addConstraints:self.constraints];
+    
   }
 }
 
 - (void)layoutSubviews {
   [super layoutSubviews];
   // TODO(rcacheaux): Use autolayout.
-  /*
+  
   CGPoint centerPoint = CGPointMake(CGRectGetMidX(self.bounds),CGRectGetMidY(self.bounds));
   
   float leftPadding = 25.0f;
   float topPadding = 30.0f;
-  self.connectionsQuizStartView.frame = CGRectMake(0.0f, 10.0f, 320.0f, 200.0f);
-  self.connectionsQuizPaperImage.frame = CGRectMake(0.0f, 10.0f, 320.0f, 200.0f);
-  self.connectionsQuizTitle.frame = CGRectMake(leftPadding, topPadding, 280.0f, 20.0f);
-  self.connectionsQuizNumberOfConnectionsLabel.frame = CGRectMake(leftPadding, topPadding+20.0f, 280.0f, 20.0f);
-  self.connectionsQuizImagePreviewCollection.frame = CGRectMake(leftPadding+10.0f, topPadding+60.0f, 250.0f, 60.0f);
-  self.connectionsQuizButton.frame = CGRectMake(leftPadding+100, topPadding+130.0f, 150.0f, 52.0f);
-  */
+  //self.connectionsQuizStartView.frame = CGRectMake(0.0f, 10.0f, 320.0f, 200.0f);
+  //self.connectionsQuizPaperImage.frame = CGRectMake(0.0f, 10.0f, 320.0f, 200.0f);
+  //self.connectionsQuizTitle.frame = CGRectMake(leftPadding, topPadding, 280.0f, 20.0f);
+  //self.connectionsQuizNumberOfConnectionsLabel.frame = CGRectMake(leftPadding, topPadding+20.0f, 280.0f, 20.0f);
+  //self.connectionsQuizImagePreviewCollection.frame = CGRectMake(leftPadding+10.0f, topPadding+60.0f, 250.0f, 60.0f);
+  //self.connectionsQuizButton.frame = CGRectMake(leftPadding+100, topPadding+130.0f, 150.0f, 52.0f);
+
 }
 
 #pragma mark Factory Methods
