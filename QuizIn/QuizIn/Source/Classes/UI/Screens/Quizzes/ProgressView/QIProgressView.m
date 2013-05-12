0594eb2beda9d228fd29017dd48231d159d56c9d
Rick <rkuhlman@gmail.com>
2013-05-12 09:53:54 -0500

    Continue on New ProgressView

--- QuizIn/QuizIn/Source/Classes/UI/Screens/Quizzes/ProgressView/QIProgressView.m
+++ QuizIn/QuizIn/Source/Classes/UI/Screens/Quizzes/ProgressView/QIProgressView.m
@@ -6,6 +6,8 @@
 @property(nonatomic, strong) UILabel *progressLabel;
 @property(nonatomic, strong) UIProgressView *progressView;
 @property(nonatomic, strong,readwrite) UIButton *exitButton;
+@property(nonatomic, strong) NSMutableArray *progressViewConstraints;
+
 @end
 
 @implementation QIProgressView
@@ -22,7 +24,12 @@
       _progressLabel = [self newProgressLabel];
       _progressView = [self newProgressView];
       _exitButton = [self newExitButton];
-
+      
+      //TODO rkuhlman
+      _quizProgress = 1;
+      _numberOfQuestions = 11;
+      
+      [self constructViewHierarchy];
     }
     return self;
 }
@@ -56,6 +63,35 @@
 
 - (void)updateConstraints {
   [super updateConstraints];
+  
+  if (!self.progressViewConstraints) {
+    
+    //ProgressView Constraints
+    self.progressViewConstraints = [NSMutableArray array];
+    NSDictionary *quizProgressViews =
+    NSDictionaryOfVariableBindings(_progressLabel, _progressView, _exitButton);
+    
+    NSString *quizProgressHorizontalFromLeft =
+    @"H:|-10-[_progressLabel]-8-[_progressView]-8-[_exitButton(==44)]-10-|";
+    NSArray *quizProgressHorizontalConstraintsLeft =
+    [NSLayoutConstraint constraintsWithVisualFormat:quizProgressHorizontalFromLeft
+                                            options:NSLayoutFormatAlignAllCenterY
+                                            metrics:nil
+                                              views:quizProgressViews];
+    NSLayoutConstraint *centerProgressBar = [NSLayoutConstraint constraintWithItem:_progressView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0.0f];
+    
+    NSString *quizProgressVertical = @"V:|-10-[_exitButton(==44)]";
+    NSArray *quizProgressVerticalConstraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:quizProgressVertical
+                                            options:0
+                                            metrics:nil
+                                              views:quizProgressViews];
+    [self.progressViewConstraints addObjectsFromArray:@[centerProgressBar]];
+    [self.progressViewConstraints addObjectsFromArray:quizProgressHorizontalConstraintsLeft];
+    [self.progressViewConstraints addObjectsFromArray:quizProgressVerticalConstraints];
+    [self addConstraints:self.progressViewConstraints];
+  }
+  
 }
 
 #pragma mark Strings
